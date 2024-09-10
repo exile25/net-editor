74274e52fba97392f4b321b4e0905ecba5cf7553
exile25 <chenruiliu25@gmail.com>
2024-09-10 11:16:47 +0800

    1.2

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener(
+    'DOMContentLoaded', function() 
+    {
     const images = [
-        "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA11MZ4O.img",
-        "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msKEu.img",
-        "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msMpx.img"
+        "https://cdn.pixabay.com/photo/2024/05/23/12/24/ai-generated-8783105_1280.jpg",
+        "https://cdn.pixabay.com/photo/2024/02/21/11/42/ai-generated-8587534_1280.jpg",
+        "https://cdn.pixabay.com/photo/2024/04/09/16/30/ai-generated-8686301_1280.jpg"
     ];
 
     let currentIndex = 0;
@@ -11,7 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const nextBtn = document.getElementById('next-btn');
 
     function updateImage() {
-        imageSlider.src = images[currentIndex];
+        imageSlider.classList.add('fade-out');
+        setTimeout(()=>{
+            imageSlider.src=images[currentIndex];
+            imageSlider.classList.remove('fade-out');
+        },1000)
+       
+    }
+
+    function nextImage(){
+        currentIndex=(currentIndex+1)%images.length;
+        updateImage();
     }
 
     prevBtn.addEventListener('click', function() {
@@ -24,6 +36,13 @@ document.addEventListener('DOMContentLoaded', function() {
         updateImage();
     });
 
+    setInterval(nextImage, 5000);
     updateImage();
-});
+    }
+);
+
+function learnMore() {
+    alert("about us。。。");
+}
+
 
